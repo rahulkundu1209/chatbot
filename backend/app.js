480fcedd78bb5c4bf075d1c4e7daade6dee347a5
Arghya123r <tapaskumardas1707@gmail.com>
2025-01-29 19:51:13 +0530

    small changes

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ async function main() {
   const chatCompletion = await client.chat.completions.create({
     messages: [
     { role: 'system', content : 'Assume the role of a web development expert specializing in the MERN stack (MongoDB, Express.js, React.js, and Node.js). Respond to questions and provide explanations, code examples, links to relevant websites and best practices related to web development and the MERN stack. Strong Rule: If you are asked about other topics do not read it and tell them "Sorry I cannot answer this question as it is not relevant to the topic"'},
-    { role: 'user', content: 'how to make a slide anumation in react, HTML, and CSS' }],
+    { role: 'user', content: 'how to make a slide anumation in react, HTML, and CSS' }],//<--- put the message here after removing this string
     model: 'llama-3.3-70b-versatile',
   });
 
